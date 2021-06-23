acb2c048d49c6d84750a3d8939644804cd2015d3
Ted Zyzsdy <zyzsdy@gmail.com>
2021-06-24 01:03:51 +0800

    增加提示系统

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -38,6 +38,43 @@
             </div>
         </div>
     </nav>
+    <div class="modal fade" id="puzzleTips" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="puzzleTipsDialogHeader" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-fullscreen-md-down modal-lg">
+            <div class="modal-content bg-dark text-light">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="puzzleTipsDialogHeader">提示</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-info mb-4">
+                        提示币数量： {{tipsCoin}} c
+                    </div>
+                    <div class="container-fluid" v-if="tipsCoin > 0">
+                        <div v-for="tip in answerTips" :key="tip.tips_id" class="mb-4">
+                            <div class="d-flex justify-content-between">
+                                <div><h5>提示{{tip.tip_num}}：{{tip.title}}
+                                    <span class="badge bg-secondary" v-if="tip.is_open == 0">消耗 {{tip.cost}}c 解锁</span>
+                                    <span class="badge bg-success" v-else>已解锁</span>
+                                </h5>
+                                </div><div>
+                                <button v-if="tip.is_open == 0" class="btn btn-secondary" @click="unlockTip(tip.tip_num)">解锁</button></div>
+                            </div>
+                            <div>{{tip.content}}</div>
+                        </div>
+                        <div v-if="answerTips.length == 0">
+                            暂时没有提示，说不定过一段时间回来看看会有。
+                        </div>
+                    </div>
+                    <div v-else>
+                        没有提示币怎么要提示啊。（24小时后每小时会获得1枚提示币）
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">关闭</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -47,14 +84,19 @@
 <script setup>
 import gConst from '../globalconst'
 import { useRoute, useRouter } from 'vue-router'
+import { fetchPostWithSign, defaultApiErrorAction } from '../utils/FetchPost'
 import { ref } from '@vue/reactivity';
 import { onMounted } from '@vue/runtime-core';
+import { Modal } from 'bootstrap';
 
 const route = useRoute();
 const router = useRouter();
 
 const skipPrologue = ref(false);
 const answer = ref("");
+
+const answerTips = ref([]);
+const tipsCoin = ref(0.0);
 
 
 onMounted(() => {
@@ -78,8 +120,8 @@ function sendAnswer() {
     answer.value = "";
 }
 
-function showTip() {
-    let pid = route.params.pid;
+async function showTip() {
+    let pid = parseInt(route.params.pid);
     if (!pid) {
         gConst.status.emit("message", {
             type: "info",
@@ -88,7 +130,51 @@ function showTip() {
         return;
     }
 
-    console.log("showTip Clicked");
+    await reloadTip(pid);
+
+    var tipDialog = new Modal(document.getElementById("puzzleTips"));
+    tipDialog.show();
+}
+
+async function unlockTip(tip_num){
+    let pid = parseInt(route.params.pid);
+    if (!pid) {
+        gConst.status.emit("message", {
+            type: "info",
+            message: "题目ID不正确"
+        });
+        return;
+    }
+
+    let api = gConst.apiRoot + "/play/unlock-tips";
+    let res = await fetchPostWithSign(api, {
+        pid,
+        tip_num
+    });
+    let data = await res.json();
+
+    if (data['status'] == 1) {
+        await reloadTip(pid);
+    } else {
+        defaultApiErrorAction(data);
+    }
+}
+
+async function reloadTip(pid){
+    let api = gConst.apiRoot + "/play/get-tips";
+    let res = await fetchPostWithSign(api, {
+        pid
+    });
+    let data = await res.json();
+
+    if (data['status'] == 1) {
+        if (data.puzzle_tips) {
+            answerTips.value = data.puzzle_tips
+        }
+        tipsCoin.value = data.tips_coin;
+    } else {
+        defaultApiErrorAction(data);
+    }
 }
 
 function showAnswerHistory() {
